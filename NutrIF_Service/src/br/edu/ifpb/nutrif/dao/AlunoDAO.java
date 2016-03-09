9727642cc07cd1972aedef0d79fadcf9562a430f
LADOSSIFPB <ladossifpb@gmail.com>
2016-03-08 22:13:42 -0300

    NutrIF
    - Integração entre o Commons e Service;
    - Criação de classe de exemplo.

---
+++ NutrIF_Service/src/br/edu/ifpb/nutrif/dao/AlunoDAO.java
@@ -0,0 +1,28 @@
+package br.edu.ifpb.nutrif.dao;
+
+import java.util.List;
+
+import br.edu.ifpb.nutrif.exception.SQLExceptionNutrIF;
+import br.edu.ladoss.entity.Aluno;
+
+public class AlunoDAO extends AbstractDAO<Integer, Aluno> {
+
+	@Override
+	public int delete(Integer pk) throws SQLExceptionNutrIF {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Aluno> getAll() throws SQLExceptionNutrIF {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Aluno getById(Integer pk) throws SQLExceptionNutrIF {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
