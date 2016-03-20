5ca0cbfde6f103e68f3e92583bb01cc6319aeda3
José Renan <joserenansl99@gmail.com>
2016-03-20 16:55:04 -0300

    '-'

--- nutrif-web/js/config/valuesConfig.js
+++ nutrif-web/js/config/valuesConfig.js
@@ -5,8 +5,8 @@ angular.module("NutrifApp").value("config", {
 		var _protocol = "http://"
 		var _externalDNS = "ladoss.com.br";
 		var _externalIP = "200.129.68.181";
-		var _internalIP = "localhost";
-		var _port = "8080";
+		var _internalIP = "192.168.1.245";
+		var _port = "8773";
 		var _context = "/NutrIF_Sevice";
 
 		var url = location.href;
