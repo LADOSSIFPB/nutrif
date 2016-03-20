878deba46706ca6c3bc4c96e45498b5729044e43
José Renan <joserenansl99@gmail.com>
2016-03-20 16:21:49 -0300

    Corrigido mais um bug com o IP --'

--- nutrif-web/js/config/valuesConfig.js
+++ nutrif-web/js/config/valuesConfig.js
@@ -2,18 +2,19 @@ angular.module("NutrifApp").value("config", {
 
 	baseUrl: function() {
 
-		var _externalDNS = "http://ladoss.com.br";
-		var _externalIP = "http://200.129.68.181";
-		var _internalIP = "http://192.168.1.245";
+		var _protocol = "http://"
+		var _externalDNS = "ladoss.com.br";
+		var _externalIP = "200.129.68.181";
+		var _internalIP = "192.168.1.245";
 		var _port = "8773";
 		var _context = "/NutrIF_Sevice";
 
 		var url = location.href;
 
 		if (url.indexOf(_externalDNS) >= 0 || url.indexOf(_externalIP) >= 0)
-			return _externalDNS + ":" + _port + _context;
+			return _protocol + _externalDNS + ":" + _port + _context;
 		else
-			return _internalIP + ":" + _port + _context;
+			return _protocol + _internalIP + ":" + _port + _context;
 
 	}
 
