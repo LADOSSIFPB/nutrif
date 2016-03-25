26aca2e7f41ff9e3a4d8c667b7d42f4808a68789
LADOSS IFPB <ladossifpb@gmail.com>
2016-03-25 15:18:37 -0300

    Controle de acesso
    - Ajuste no controle de acesso para Pessoa.

---
+++ nutrif-commons/src/br/edu/ladoss/entity/PessoaAcesso.java
@@ -0,0 +1,20 @@
+package br.edu.ladoss.entity;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class PessoaAcesso extends Pessoa {
+
+	private static final long serialVersionUID = 8011399615861874456L;
+
+	@XmlElement
+	@Override
+	public String getSenha() {
+		return super.getSenha();
+	}
+	
+	@XmlElement
+	@Override
+	public String getKeyAuth() {
+		return super.getKeyAuth();
+	}
+}
