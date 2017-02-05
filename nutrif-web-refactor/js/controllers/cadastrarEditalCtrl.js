d5f980a1261e4c2dd7a977485569c0976f5342b4
Fanny <fannyvieiira@hotmail.com>
2017-02-05 18:07:43 -0200

    - Configuração de data, modificada para português.

--- nutrif-web-refactor/js/controllers/cadastrarEditalCtrl.js
+++ nutrif-web-refactor/js/controllers/cadastrarEditalCtrl.js
@@ -121,4 +121,26 @@ angular.module('NutrifApp').controller('cadastrarEditalCtrl', function ($scope, 
 
 		carregarEventos();
 		carregarCampi();
+})
+.config(function($mdDateLocaleProvider) {
+  // Example of a Spanish localization.
+  $mdDateLocaleProvider.months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 
+                                  'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  $mdDateLocaleProvider.shortMonths = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 
+                                  'Jul', 'Ago', 'Sep', 'Out', 'Nov', 'Dez'];
+  $mdDateLocaleProvider.days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 
+                                'Quinta', 'Sexta', 'Sábado'];
+  $mdDateLocaleProvider.shortDays = ['Do', 'Se', 'Te', 'Qua', 'Qui', 'Sex', 'Sab'];
+  // Can change week display to start on Monday.
+  $mdDateLocaleProvider.firstDayOfWeek = 1;
+  // Optional.
+  //$mdDateLocaleProvider.dates = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,
+  //                               20,21,22,23,24,25,26,27,28,29,30,31];
+  // In addition to date display, date components also need localized messages
+  // for aria-labels for screen-reader users.
+  $mdDateLocaleProvider.weekNumberFormatter = function(weekNumber) {
+    return 'Semana ' + weekNumber;
+  };
+  $mdDateLocaleProvider.msgCalendar = 'Calendario';
+  $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
 });
