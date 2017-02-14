8413f929a550b54c1199be850da4b3346a99f2e0
FannyVieira <fannyvieiira@hotmail.com>
2017-02-14 15:23:04 -0300

    - Criação de Interceptor

--- nutrif-web-refactor/js/services/interceptor.js
+++ nutrif-web-refactor/js/services/interceptor.js
@@ -1,25 +1,21 @@
-angular.module('NutrifApp').factory('Interceptor',
-		function () {
+angular.module('NutrifApp').factory('Interceptor',function () {
 		
-		$inject = ['$q','$state'];
-		
-		
-		function success(response) {
-            return response;
-		}
+	$inject = ['$q','$state'];
 
-		function error(response) {
-		     if(response.status === 401) {
-		            $state.go('login.gerenciamento');
-		            return $q.reject(response);
-		       }
-		       else {
-		            return $q.reject(response);
-		        }
+	function success(response) {
+		return response;
+	}
+
+	function error(response) {
+		if(response.status === 401) {
+				$state.go('login.gerenciamento');
+				return $q.reject(response);
 		}
-	
-		return function(promise) {
-	            return promise.then(success, error);
+		else {
+				return $q.reject(response);
 		}
-		
+	}
+	return function(promise) {
+		return promise.then(success, error);
+	}		
 });
