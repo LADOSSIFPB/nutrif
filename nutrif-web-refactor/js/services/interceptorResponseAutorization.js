a8d552f418a396882fd00fb18dffcc305560af5f
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2017-02-17 15:16:03 -0300

    Interceptor do response com erro.
    - Retorno para a aplicação geral do response com erro.

--- nutrif-web-refactor/js/services/interceptorResponseAutorization.js
+++ nutrif-web-refactor/js/services/interceptorResponseAutorization.js
@@ -1,29 +1,27 @@
-	angular.module('NutrifApp').factory('interceptorResponseAutorization', function ($q,$injector,userService) {
-	    return {
+angular.module('NutrifApp').factory('interceptorResponseAutorization', function ($q, $injector, userService) {
+    return {
 
-	       response: function (response) {
-	            // Fluxo normal das respostas do servidor.
-	                return response || $q.when(response);
-	        },
+       response: function (response) {
+            // Fluxo normal das respostas do servidor.
+            return response || $q.when(response);
+        },
 
 			responseError: function (response) {
-						
+
 				// Verificar o retorno de erro da resposta para status: UNAUTHORIZED-401.
 				var $ERRO_UNAUTHORIZED = 401;
 				$status = response.status;
 
 				if($status === $ERRO_UNAUTHORIZED){
-					console.log("ResponseError: UNAUTHORIZED-401");
-					// Redirecionar: falta configurar o retorno
-					
+
 					// Remove dados do usuário do cookie.
-			        userService.removeUser();
+					userService.removeUser();
+
+					// Redireciona para o Login.
 					$injector.get('$state').go('login.gerenciamento');
-					
-					return;
 				}
 
 				return $q.reject(response);
-	        }
-	    };
-	});
+			}
+    };
+});
