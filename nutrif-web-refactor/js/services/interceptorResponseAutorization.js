a79cc084948b4332382eb3a5633e6d14e5d181ce
FannyVieira <fannyvieiira@hotmail.com>
2017-02-17 14:31:15 -0300

    - Ajuste na transição de página

--- nutrif-web-refactor/js/services/interceptorResponseAutorization.js
+++ nutrif-web-refactor/js/services/interceptorResponseAutorization.js
@@ -1,24 +1,29 @@
-	angular.module('NutrifApp').factory('interceptorResponseAutorization', function ($q, $state) {
+	angular.module('NutrifApp').factory('interceptorResponseAutorization', function ($q,$injector,userService) {
 	    return {
 
-	        response: function (response) {
+	       response: function (response) {
 	            // Fluxo normal das respostas do servidor.
-	            return response;
+	                return response || $q.when(response);
 	        },
 
-					responseError: function (response) {
+			responseError: function (response) {
 						
-							// Verificar o retorno de erro da resposta para status: UNAUTHORIZED-401.
-							var $ERRO_UNAUTHORIZED = 401;
-							$status = response.status;
+				// Verificar o retorno de erro da resposta para status: UNAUTHORIZED-401.
+				var $ERRO_UNAUTHORIZED = 401;
+				$status = response.status;
 
-							if($status === $ERRO_UNAUTHORIZED){
-									console.log("ResponseError: UNAUTHORIZED-401");
-									// Redirecionar: falta configurar o retorno.
-									$state.transitionTo('login.gerenciamento', {reload: true});
-							}
+				if($status === $ERRO_UNAUTHORIZED){
+					console.log("ResponseError: UNAUTHORIZED-401");
+					// Redirecionar: falta configurar o retorno
+					
+					// Remove dados do usuário do cookie.
+			        userService.removeUser();
+					$injector.get('$state').go('login.gerenciamento');
+					
+					return;
+				}
 
-							return $q.reject(response);
+				return $q.reject(response);
 	        }
 	    };
 	});
