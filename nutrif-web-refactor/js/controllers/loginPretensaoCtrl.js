561add153bdc397dba5e4bee571dea980eeb0743
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2017-03-27 22:07:52 -0300

    Acesso aluno na pretensão
    - Acesso aluno na pretensão com atualização dos dados: email, período,
    turma e turno.

--- nutrif-web-refactor/js/controllers/loginPretensaoCtrl.js
+++ nutrif-web-refactor/js/controllers/loginPretensaoCtrl.js
@@ -1,51 +1,56 @@
-angular.module("NutrifApp").controller("loginPretensaoCtrl", function (userService, $state, alunoService, $mdToast, $scope) {
+angular.module("NutrifApp").controller("loginPretensaoCtrl", function (userService, alunoService, $state, $mdToast, $scope) {
+    $scope.acesso = false;
+    
+    var ACESSO_CONFIRMADO = true;
 
-	
-	$scope.acesso = false;
-		
-	this.verificarAcesso = function(matricula){
-		
+    this.verificarAcesso = function (matricula) {
+
         alunoService.verificarAcesso(matricula)
-	        .success(function (data, status) {
-	           $scope.aluno = data;
-	           if($scope.aluno.acesso == true)
-	        	   $scope.acesso = true;
-	     
-	        })
-	        .error(function (data, status) {
-	
-	            delete $scope.matricula;
-	            var _message = "";
-	
-	            if (!data) {
-	                _message = "Aluno inexistente"
-	            } else {
-	                _message = data.mensagem
-	            }
-	
-	            $mdToast.show(
-	                $mdToast.simple()
-	                .textContent(_message)
-	                .position('top right')
-	                .action('OK')
-	                .hideDelay(6000)
-	            );
-	            return false;
-	        });
-	}
-	
+            .success(function (data, status) {
+                $scope.aluno = data;
+                if ($scope.aluno.acesso == ACESSO_CONFIRMADO){
+                    
+                    // Direcionar para a tela com o campo de senha.
+                } else {
+                    
+                    // Direcionar para a atualização dos dados.
+                }
+            })
+            .error(function (data, status) {
+                delete $scope.matricula;
+                var _message = "";
+
+                if (!data) {
+                    _message = "Aluno inexistente"
+                } else {
+                    _message = data.mensagem
+                }
+
+                $mdToast.show(
+                    $mdToast.simple()
+                    .textContent(_message)
+                    .position('top right')
+                    .action('OK')
+                    .hideDelay(6000)
+                );
+                return false;
+            });
+    }
+
     this.fazerLogin = function (aluno) {
         alunoService.fazerLogin(aluno)
             .success(function (data, status) {
-            	$scope.aluno = data;
-            	
-            	console.log(data);
-            	
+                $scope.aluno = data;
+
+                console.log(data);
+
                 userService.storeUser(data);
-                if($scope.aluno.acesso == true){
-                	$state.go("pretensao.listar-pretensao");
-                }else{
-                	 $state.transitionTo("pretensao.atualizar-dados-aluno",{matricula:$scope.aluno.matricula});
+                if ($scope.aluno.acesso == true) {
+                    $state.go("pretensao.listar-pretensao");
+                } else {
+                    $state.transitionTo("pretensao.atualizar-dados-aluno", {
+                        matricula: $scope.aluno.matricula
+                    });
                 }
             })
 
