0a2a5c58562bbc7fa8c7c893120483ebd06d6f06
Marcos Silva <vinicius_marcosmartins@hotmail.com>
2017-08-30 15:20:26 -0300

    Criação da classe Deteccao
    - Pequeno delay adicionado durante a captura da imagem

--- nutrif-face-detection/resources/deteccao_facial.py
+++ nutrif-face-detection/resources/deteccao_facial.py
@@ -2,15 +2,14 @@ from flask import g
 from flask_restful import Resource, marshal_with, abort
 from common.auth import auth
 from models.aluno import Aluno, aluno_json
-import webcam
-import base64
-from time import sleep
+from webcam import Deteccao
 
 # GET /detectaface/
 class DeteccaoFacialResource(Resource):
 
     @marshal_with(aluno_json)
     def get(self, matricula):
-        foto = webcam.main()
+        d = Deteccao()
+        foto = d.detectar()
         aluno = Aluno(matricula, foto)
         return (aluno, 200)
